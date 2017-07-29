db4ad37d047f14e1386477095c6b81d7397e3710
elieohanian <elieohanian@isae.edu.lb>
2017-07-29 12:40:48 +0300

    Create Brand Elie Ohanian

---
+++ app/src/main/java/com/zaita/aliyounes/gsbvc2017/ajouter_brand.java
@@ -0,0 +1,13 @@
+package com.zaita.aliyounes.gsbvc2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ajouter_brand extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter_brand);
+    }
+}
