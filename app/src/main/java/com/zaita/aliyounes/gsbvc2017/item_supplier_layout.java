8da78c0d46a3a882243a7acb5d000d868c982b1d
elieohanian <elieohanian@isae.edu.lb>
2017-08-12 12:33:36 +0300

    Item Supplier Layout

---
+++ app/src/main/java/com/zaita/aliyounes/gsbvc2017/item_supplier_layout.java
@@ -0,0 +1,13 @@
+package com.zaita.aliyounes.gsbvc2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class item_supplier_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_supplier_layout);
+    }
+}
