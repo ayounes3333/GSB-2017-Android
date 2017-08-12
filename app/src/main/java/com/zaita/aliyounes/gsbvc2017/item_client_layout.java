f7fc539900a82d517f7d51119d087681af1d4422
elieohanian <elieohanian@isae.edu.lb>
2017-08-12 12:21:20 +0300

    Item Client Layout

---
+++ app/src/main/java/com/zaita/aliyounes/gsbvc2017/item_client_layout.java
@@ -0,0 +1,13 @@
+package com.zaita.aliyounes.gsbvc2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class item_client_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_client_layout);
+    }
+}
