3a59079445c93d9ddbd2aa83dfb88474e777f02c
Ali Younes <ayounes3333@gmail.com>
2017-08-15 20:37:20 +0300

    Minor bug fixes

--- app/src/main/java/com/zaita/aliyounes/gsbvc2017/fragments/ClientsFragment.java
+++ app/src/main/java/com/zaita/aliyounes/gsbvc2017/fragments/ClientsFragment.java
@@ -1,10 +1,8 @@
 package com.zaita.aliyounes.gsbvc2017.fragments;
 
-import android.app.Activity;
-
 /**
  * Created by Chawach on 8/14/2017.
  */
 
-public class ClientsFragment extends Activity {
+public class ClientsFragment {
 }
