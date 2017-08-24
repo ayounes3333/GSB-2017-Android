df2ea3ee6c42e8dc48c2d36cefa9b39c0e36201c
elieohanian <elieohanian@isae.edu.lb>
2017-08-24 16:17:11 +0300

    - fill spinners - invoice - transfer

--- app/src/main/java/com/zaita/aliyounes/gsbvc2017/activities/transfer.java
+++ app/src/main/java/com/zaita/aliyounes/gsbvc2017/activities/transfer.java
@@ -1,17 +1,17 @@
 package com.zaita.aliyounes.gsbvc2017.activities;
 
-        import android.support.design.widget.TextInputLayout;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.CheckBox;
-        import android.widget.Spinner;
-        import android.widget.Toast;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-        import com.zaita.aliyounes.gsbvc2017.R;
+import com.zaita.aliyounes.gsbvc2017.R;
 
-public class Ajouter_transaction extends AppCompatActivity {
+public class transfer extends AppCompatActivity {
 
     private TextInputLayout textInput_OrdNum;
     private TextInputLayout textInput_OrdQty;
@@ -19,6 +19,7 @@ public class Ajouter_transaction extends AppCompatActivity {
     private Spinner spinner_Brcode;
     private Spinner spinner_Prcode;
     private Spinner spinner_Trcode;
+    private Spinner spinner_Brcodeto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +35,7 @@ public class Ajouter_transaction extends AppCompatActivity {
         spinner_Trcode      = (Spinner) findViewById(R.id.spinner_Trcode);
         spinner_Prcode      = (Spinner) findViewById(R.id.spinner_Prcode);
         spinner_Brcode      = (Spinner) findViewById(R.id.spinner_Brcode);
+        spinner_Brcodeto    = (Spinner) findViewById(R.id.spinner_Brcodeto);
 
 
 
@@ -42,7 +44,7 @@ public class Ajouter_transaction extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(!checkFields())
-                    Toast.makeText(Ajouter_transaction.this, "Erreur", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(transfer.this, "Erreur", Toast.LENGTH_SHORT).show();
                 else
                     addTransaction();
             }
@@ -53,7 +55,7 @@ public class Ajouter_transaction extends AppCompatActivity {
         if( textInput_OrdNum.getEditText()      != null &&
                 textInput_OrdQty.getEditText()  != null &&
                 textInput_OrdDate.getEditText() != null
-              ) {
+                ) {
             if (textInput_OrdNum.getEditText().getText().toString().equalsIgnoreCase("")) {
                 textInput_OrdNum.setError("Le Numero est obligatoire");
                 return false;
@@ -74,7 +76,10 @@ public class Ajouter_transaction extends AppCompatActivity {
                 Toast.makeText(this, "Selectionner un Product", Toast.LENGTH_SHORT).show();
             }
             if(spinner_Brcode.getSelectedItem() == null) {
-                Toast.makeText(this, "Selectionner un Branch", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Selectionner From  Branch", Toast.LENGTH_SHORT).show();
+            }
+            if(spinner_Brcodeto.getSelectedItem() == null) {
+                Toast.makeText(this, "Selectionner To Branch", Toast.LENGTH_SHORT).show();
             }
             return true;
         }
