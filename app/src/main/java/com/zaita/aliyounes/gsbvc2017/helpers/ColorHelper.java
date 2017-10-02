bc2c5359287cf7782828200f3b9433054ac3b4e3
ali.younes <ali.youness@gmail.com>
2017-10-02 16:57:32 +0300

    Changed Color and Icons Major UI Changes

--- app/src/main/java/com/zaita/aliyounes/gsbvc2017/helpers/ColorHelper.java
+++ app/src/main/java/com/zaita/aliyounes/gsbvc2017/helpers/ColorHelper.java
@@ -12,21 +12,14 @@ public class ColorHelper {
 
     private static int lastColor = 0;
     private static String[] mColors = {
-            "F44336", //Red
-            "E91E63", //Pink
-            "9C27B0", //Purple
-            "673AB7", //Deep Purple
-            "3F51B5", //Indigo
-            "2196F3", //Blue
-            "03A9F4", //Light Blue
-            "00BCD4", //Cyan
-            "009688", //Teal
-            "FF9800", //Orange
+            "e3ab38", //Orange
+            "a1a1a1", //Grey
+            "4bb2c5", //Blue
     };
     public static int getRandomColor() {
         int color;
         do {
-            color = Color.parseColor("#" + mColors[new Random().nextInt(9)]);
+            color = Color.parseColor("#" + mColors[new Random().nextInt(3)]);
         } while (lastColor == color);
         lastColor = color;
         return color;
@@ -34,7 +27,7 @@ public class ColorHelper {
     public static int getRandomColor(int last) {
         int color;
         do {
-            color = Color.parseColor("#" + mColors[new Random().nextInt(254)]);
+            color = Color.parseColor("#" + mColors[new Random().nextInt(3)]);
         } while (last == color);
         lastColor = color;
         return color;
